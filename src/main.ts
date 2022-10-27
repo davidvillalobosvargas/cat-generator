a6e1dab099ba441b32c1c6dced0f3f4cc52657c2
davidvillalobosvargas <davidvillalobosvargas@gmail.com>
2022-10-26 20:29:01 -0600

    add main ts for fetching cats

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
 import './style.css'
+const catImg=document.querySelector<HTMLImageElement>('#cat-img')
+const nextCatBtn=document.getElementById('next-cat-btn') as HTMLButtonElement
+
+document.addEventListener('DOMContentLoaded',fetchCatUrl)
+
+nextCatBtn.addEventListener('click',fetchCatUrl)
+
+function fetchCatUrl():void
+{
+    fetch('https://api.thecatapi.com/v1/images/search')
+    .then(response=>response.json())
+    .then(data=>{
+        if (catImg){
+            catImg.src=data[0].url
+        }
+    })
+    .catch(error=>console.log(error))
+}
